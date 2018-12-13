8eda04e12b3d844488865c3a3b8d54a619292284
Fabien Brulport <fabien.brulport@gmail.com>
2018-12-13 23:22:29 +0100

    tranform notebook into python scripts

---
+++ real_example.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from geopy import geocoders
+from mpl_toolkits.basemap import Basemap
+from geopy.exc import GeocoderTimedOut
+
+from graph import Node, Graph
+from aco import ACO
+
+
+def do_geocode(address, geolocator):
+    try:
+        return geolocator.geocode(address)
+    except GeocoderTimedOut:
+        return do_geocode(address, geolocator)
+
+
+# Retrieve cities coordinates and plot (may take some time)
+cities = ['Amsterdam', 'Athens', 'Belfast', 'Belgrade', 'Berlin', 'Bern', 'Brussels', 'Bucharest', 'Budapest',
+          'Copenhagen', 'Dublin', 'Helsinki', 'Kiev', 'Lisbon', 'Ljubljana', 'London', 'Luxembourg', 'Madrid', 'Oslo',
+          'Paris',
+          'Prague', 'Reykjavík', 'Riga', 'Rome', 'Sofia', 'Tallinn', 'Vienna', 'Vilnius', 'Warsaw', 'Zagreb']
+geolocator = geocoders.ArcGIS()
+latitudes = []
+longitudes = []
+
+for city in cities:
+    geo = do_geocode(city, geolocator)
+    latitudes.append(geo.latitude)
+    longitudes.append(geo.longitude)
+
+
+def plot_map(ax):
+    basemap = Basemap(projection='mill', llcrnrlon=min(longitudes) - 1, urcrnrlon=max(longitudes) + 1,
+                      llcrnrlat=min(latitudes) - 1, urcrnrlat=max(latitudes) + 1, resolution='i', ax=ax)
+    basemap.drawcoastlines()
+    basemap.fillcontinents()
+    basemap.drawmapboundary()
+    basemap.drawcountries()
+    return basemap
+
+
+fig, ax = plt.subplots()
+basemap = plot_map(ax)
+x, y = basemap(longitudes, latitudes)
+basemap.scatter(x, y, marker='o', zorder=2)
+plt.show()
+
+# Create the graph
+nodes = []
+for i in range(len(cities)):
+    nodes.append(Node(x[i], y[i]))
+
+graph = Graph(nodes)
+
+# Solve for 100 iterations
+aco = ACO(nodes)
+paths, distances = aco.solve(rho=0.5, n_iterations=100, verbose=True)
+
+# Plot results
+fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+basemap = plot_map(axs[0])
+x, y = basemap(longitudes, latitudes)
+basemap.scatter(x, y, marker='o', zorder=2)
+
+anim = graph.plot_paths(paths, aco.pheromones, distances, fig)
+plt.show()
---
+++ simple_example.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from graph import Node, Graph
+from aco import ACO
+
+# # Simple example
+
+# Create and plot the graph
+n_nodes = 30
+
+np.random.seed(0)
+coordinates = np.random.rand(n_nodes, 2)*10
+
+nodes = []
+for i in range(n_nodes):
+    nodes.append(Node(coordinates[i, 0], coordinates[i, 1]))
+
+graph = Graph(nodes)
+fig, ax = graph.plot_graph()
+plt.show()
+
+# Solve for 100 iterations
+aco = ACO(nodes)
+paths, distances = aco.solve(rho=0.5, n_iterations=100, verbose=True)
+
+# Plot results
+anim = graph.plot_paths(paths, aco.pheromones, distances)
+plt.show()
+
+
